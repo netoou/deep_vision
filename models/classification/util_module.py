558516d87618751628d88747f57db4bf0500a728
JaeBeome <netoou@naver.com>
2019-12-27 14:25:50 +0900

    ADD SwishActivation at util_module.py

--- models/classification/util_module.py
+++ models/classification/util_module.py
@@ -6,3 +6,11 @@ class Flatten(Module):
     def forward(self, input):
         batch, _, _, _ = input.size()
         return input.reshape(batch, -1)
+
+class SwishActivation(Module):
+    def __init__(self, activation):
+        super(SwishActivation, self).__init__()
+        self.activation = activation
+
+    def forward(self, input):
+        return input * self.activation(input)
